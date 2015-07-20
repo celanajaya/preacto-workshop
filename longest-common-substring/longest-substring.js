c675e7d28ba5964ea091efc8273ebc556e01e5db
celanajaya <petermsteele@gmail.com>
2015-07-20 11:25:37 -0400

    lcs passes spec, but not all cases

--- longest-common-substring/longest-substring.js
+++ longest-common-substring/longest-substring.js
@@ -1,3 +1,10 @@
 function longestCommonSubstring(str1, str2) {
-
-}
+	return str1.split("").reduce(function(prev, next){
+		for (var i = 0; i < str2.length; i++){
+			if (str2[i] === next) {
+				return prev + next;
+			}
+		}
+		return prev = ''
+	}, '');
+};
